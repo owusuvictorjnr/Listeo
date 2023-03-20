f28716e0f8e4ba13f24e35ebbb9544f340d441eb
owusuvictorjnr <owusuvictor80@gmail.com>
2023-03-20 15:37:31 +0000

    screen class

--- components/Layout.js
+++ components/Layout.js
@@ -11,7 +11,7 @@ export default function Home({ title, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <div className="min-h-screen flex flex-col justify-between">
         <header>
           <Navbar />
         </header>
