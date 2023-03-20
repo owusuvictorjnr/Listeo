b253fc9b3bcf115dd7c3b2f61adf55647fe9be53
owusuvictorjnr <owusuvictor80@gmail.com>
2023-03-20 15:38:49 +0000

    Wrap Home around Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Layout from '@/components/Layout';
+import Home from '@/components/Home';
 
 const index = () => {
-  return <Layout>index</Layout>;
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 };
 
 export default index;
