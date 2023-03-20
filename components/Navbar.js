56a6f5356c7fe50ca68dc01d93aaf6bb318fd7fa
owusuvictorjnr <owusuvictor80@gmail.com>
2023-03-20 15:37:56 +0000

    Navbar items

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { TfiLocationPin } from 'react-icons/tfi';
+import { BsCart2 } from 'react-icons/bs';
+import { IoLogInOutline } from 'react-icons/io5';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="h-[5rem] bg-gray-100 flex justify-between shadow-lg items-center md:px-20 lg:justify-center lg:gap-[60rem]">
+      <div className="flex gap-2 items-center ">
+        <TfiLocationPin size={60} color="red" className="flex items-center" />
+        <h1>listeo</h1>
+      </div>
 
-export default Navbar
+      <div className="flex gap-10 ">
+        <BsCart2
+          size={55}
+          color="red"
+          className="rounded-full bg-red-600/25 py-4 px-4 text-white"
+        />
+        <div className="flex items-center">
+          <IoLogInOutline size={30} />
+          sign in
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
